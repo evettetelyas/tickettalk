91721e512c2993b17d839eaae8e477d02863ebf8
Evette Telyas <etelyas@gmail.com>
2019-10-29 16:13:15 -0600

    fixed chat JS template

--- app/assets/javascripts/channels/room_channel.js
+++ app/assets/javascripts/channels/room_channel.js
@@ -14,7 +14,8 @@ $(function() {
 		{
 		  received: function(data) {
 			var content = messageTemplate.children().clone(true, true);
-			content.find('[data-role="user-avatar"]').attr('src', data.user_avatar_url);
+			// content.find('[data-role="user-avatar"]').attr('src', data.user_avatar_url);
+			content.find('[data-role="user-username"]').attr('href', data.username_url).text(data.username);
 			content.find('[data-role="message-text"]').text(data.message);
 			content.find('[data-role="message-date"]').text(data.updated_at);
 			$element.append(content);
