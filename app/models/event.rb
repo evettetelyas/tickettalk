b416fee5737da9b8d8a175a022942b3ff35c4f40
Evette Telyas <etelyas@gmail.com>
2019-10-28 21:04:25 -0600

    update date

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,9 @@ class Event < ApplicationRecord
     def formatted_date
         date.to_datetime.strftime('%D') if date
     end
+
+    def formatted_onsale
+        on_sale.to_datetime.localtime.strftime("%a %x %l:%M %p")
+    end
 end
 
