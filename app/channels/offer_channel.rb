271c5472a4749e7363ae5a3aa0a6457f257c743c
Evette Telyas <etelyas@gmail.com>
2019-10-30 17:18:30 -0600

    action cable

--- app/channels/offer_channel.rb
+++ app/channels/offer_channel.rb
@@ -2,8 +2,10 @@
 
 class OfferChannel < ApplicationCable::Channel
   def subscribed
-    # user = User.find params[:user]
-    stream_for "OfferChannel:#{params[:user]}"
+    user = User.find params[:user]
+    stream_for user.offers.last
+    # offer = Offer.find params[:offer]
+    # stream_for offer
     # stream_for current_user
     # offer = Offer.find params[:offer]
     # stream_for offer
