c0aa15bbc0d176518bc9c35b2c4baa22da2b26c2
Corina Allen <StarPerfect@gmail.com>
2019-10-31 11:17:23 -0600

    Update Welcome controller with about and contact actions

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,10 @@ class WelcomeController < ApplicationController
   def index
       UpcomingOnSalesJob.perform_later(current_user.interests.pluck(:keyword).uniq) if current_user
   end
+
+  def about
+  end
+
+  def contact
+  end 
 end
