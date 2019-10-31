add21f1633bc1724e84e84a461133287451b27cb
Tylor Schafer <tylorschafer@gmail.com>
2019-10-30 19:00:14 -0600

    Add room messages sad path testing

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
-  validates_presence_of :content
+  validates_presence_of :content, :rating
 
   belongs_to :user
   belongs_to :reviewer, class_name: 'User'
