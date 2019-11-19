25726f9b50e2a15df841e6025f15e679cecd5c62
Tylor Schafer <tylorschafer@gmail.com>
2019-11-18 20:58:03 -0700

    Add user dashboard facade #find_user testing

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 describe 'User dashboard' do
-  it '#seller_paypal can return a users paypal me full url' do
-    df = DashboardFacade.new
-    user = create(:user, paypal_me: 'tylor')
+  before :each do
+    @df = DashboardFacade.new
+    @user = create(:user, id: 1, username: 'tylor', paypal_me: 'tylor')
+    @offer_user = create(:user, id: 2, username: 'bob')
+  end
 
-    expect(df.seller_paypal(user.id)).to eq("https://www.paypal.me/tylor")
+  it '#seller_paypal can return a users paypal me full url' do
+    expect(@df.seller_paypal(@user.id)).to eq("https://www.paypal.me/tylor")
+  end
+
+  it '#find_user returns a user object' do
+    result_1 = @df.find_user(1, 2, 'receive')
+    expect(result_1).to eq('bob')
+
+    result_2 = @df.find_user(1, 2, 'sent')
+    expect(result_2).to eq('tylor')
   end
 end
