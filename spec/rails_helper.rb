4aac030d7ebbf7bf57b8db9e441daccbf3eb94d8
Tylor Schafer <tylorschafer@gmail.com>
2019-11-18 20:01:45 -0700

    Setup action cable testing for project

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,6 +21,7 @@ if Rails.env.production?
   abort('The Rails environment is running in production mode!')
 end
 require 'rspec/rails'
+require 'action_cable_testing/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
