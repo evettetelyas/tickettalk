2a85cbbce025c6bb4d52eaadc7b7c27e14aed6f9
Tylor Schafer <tylorschafer@gmail.com>
2019-11-18 20:47:56 -0700

    Add offer channel testing

--- app/channels/offer_channel.rb
+++ app/channels/offer_channel.rb
@@ -4,10 +4,5 @@ class OfferChannel < ApplicationCable::Channel
   def subscribed
     user = User.find params[:user]
     stream_for user
-    # offer = Offer.find params[:offer]
-    # stream_for offer
-    # stream_for current_user
-    # offer = Offer.find params[:offer]
-    # stream_for offer
   end
 end
---
+++ spec/channels/offer_channel_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe ConfirmChannel, type: :channel do
+  it 'subscribes to a stream when room id is provided' do
+    create(:user, id: 1)
+    allow_any_instance_of(ConfirmChannel).to receive(:params).and_return({ user: 1 })
+    subscribe
+
+    expect(subscription).to be_confirmed
+    expect(streams).to be_truthy
+  end
+end
