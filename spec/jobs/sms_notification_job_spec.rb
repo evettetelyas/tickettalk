d33996a6d428a5c8ae27f7ad7126025947ba3715
Tylor Schafer <tylorschafer@gmail.com>
2019-11-18 22:03:44 -0700

    Add sms notification job testing

--- spec/jobs/sms_notification_job_spec.rb
+++ spec/jobs/sms_notification_job_spec.rb
@@ -1,1 +1,17 @@
+require 'rails_helper'
 
+describe SmsNotificationJob, :vcr, type: :job do
+  before :each do
+    ActiveJob::Base.queue_adapter = :test
+    @job = SmsNotificationJob.new
+  end
+
+  describe '#perform' do
+    it 'Sends out an SMS notification if events are going on sale' do
+      event = create(:event)
+      result = @job.perform(event)
+      expect(result.class).to eq Twilio::REST::Api::V2010::AccountContext::MessageInstance
+      expect(result.body).to include('Sent from your Twilio trial account')
+    end
+  end
+end
